7c32cff03dd65f2ec4188e551760a74237d9d584
Zuka98 <32645506+Zuka98@users.noreply.github.com>
2018-10-26 20:57:49 +0100

    New Scripts added
    Date and Time working

---
+++ Doctor_App/Assets/Scripts/DateTime.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class DateTime : MonoBehaviour {
+
+    public Text date_Time;
+    private string time;
+    private string date;
+
+    void Update()
+    {   
+        time = System.DateTime.Now.ToString("hh:mm"); 
+        date = System.DateTime.Now.ToString("MM.dd.yyyy");
+        date_Time.text = time + " " + date;
+    }
+}
