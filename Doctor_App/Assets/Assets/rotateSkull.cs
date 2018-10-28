af850b0ba029b71d3f334c5333de2eeff8c00cfd
Zuka98 <32645506+Zuka98@users.noreply.github.com>
2018-10-28 23:45:21 +0000

    123
    123

---
+++ Doctor_App/Assets/Assets/rotateSkull.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    float rotationSpeed = 20;
+
+    private void OnMouseDrag()
+    {
+        float rotX = Input.GetAxis("Mouse X") * rotationSpeed * Mathf.Deg2Rad;
+        float rotY = Input.GetAxis("Mouse Y") * rotationSpeed * Mathf.Deg2Rad;
+
+        transform.RotateAround(Vector3.up, -rotX);
+        transform.RotateAround(Vector3.right, -rotY);
+    }
+}
