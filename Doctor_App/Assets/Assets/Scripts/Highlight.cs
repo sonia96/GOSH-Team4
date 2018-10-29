fc3fb60c1ad45cebb9404b1d3768c46c2f7c23df
Zuka98 <32645506+Zuka98@users.noreply.github.com>
2018-10-29 14:09:58 +0000

    Lights Function Working
    Highlighting WOrkds

---
+++ Doctor_App/Assets/Assets/Scripts/Highlight.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Highlight : MonoBehaviour {
+    private bool isOn = false;
+
+    public void Button_Click()
+    {
+        GameObject[] lights = GameObject.FindGameObjectsWithTag("SkullLight");
+        if (isOn != true)
+        {
+            for (int i = 0; i < lights.Length; i++)
+            {
+                lights[i].GetComponent<Light>().enabled = true;
+            }
+            isOn = true;
+        }
+        else{
+            for (int i = 0; i < lights.Length; i++)
+            {
+                lights[i].GetComponent<Light>().enabled = false;
+            }
+            isOn = false;
+            
+        }
+    }
+}
