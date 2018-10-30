1cff81ea2e4b17e081b7c6e7c1ae664a08c79e59
YankeZhang <32335582+YankeZhang@users.noreply.github.com>
2018-10-30 09:32:40 +0000

    Add button script

---
+++ Doctor_App/Assets/Assets/Scripts/LightControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightControl : MonoBehaviour {
+
+    public void Switch()
+    {
+        if (this.GetComponent<Light>().enabled == false)
+        {
+            this.GetComponent<Light>().enabled = true;
+        }
+        else
+        {
+            this.GetComponent<Light>().enabled = false;
+        }
+    }
+
+}
