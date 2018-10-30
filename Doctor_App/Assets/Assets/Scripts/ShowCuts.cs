eb4e3b7345842bcb581c9b9d8d4cc6b7ef7db525
Zuka98 <32645506+Zuka98@users.noreply.github.com>
2018-10-30 21:48:08 +0000

    Cut lines button - partially working
    Cut lines

---
+++ Doctor_App/Assets/Assets/Scripts/ShowCuts.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowCuts: MonoBehaviour {
+    Vector3 v1;
+    GameObject cut;
+
+    public void Button_Click()
+    {
+        cut = GameObject.FindGameObjectWithTag("cut1");
+        for (int i = 0; i < 10; i++){
+            v1 = cut.transform.position;
+            v1.z = cut.transform.position.z + i / 100f;
+            cut.transform.position = v1;
+        
+        }
+    }
+}
